5cd9c150638045a96df4f3df5eeca52be61633c4
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-08-16 16:41:16 -0300

    menu mobile, menu desktop funcionando

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,66 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const abrir = document.querySelector('.header__menu-abrir'),
+    const iconoAbrir = document.querySelector('.header__menu-abrir'),
     menuBox = document.querySelector('.header__menu-contenido'),
-    menu = document.querySelector('.nav'),
+    listaNav = menuBox.querySelector('.nav__lista'), 
     menuCerrar = document.querySelector('.header__menu-cerrar'),
     listaBtns = document.querySelectorAll('.nav__btn-abrir');
 
-    abrir.addEventListener('click', () => {
-        menuBox.style.transform = "translateX(0)";
-    });
-
-    menuCerrar.addEventListener('click', () => {
-        menuBox.style.transform = "translateX(100%)";
-    });
-
-    /* Despliegue de submenu - mobile */
-
-    function manejarEvento(btn) {
-        if (window.innerWidth <= 1280) { // Ajusta el valor según el breakpoint deseado
-            clickMobile(btn);
-        } /* else {
-            logicaDesktop();
-        } */
+    const menuToggle = (desplegar) => {
+        menuBox.style.transform = desplegar ? 'translateX(0)' : 'translateX(100%)';
     }
 
-    function modificarAlto(submenuHeight, ulPadre) {
+    iconoAbrir.addEventListener('click', () => menuToggle(true));
+    menuCerrar.addEventListener('click', () => menuToggle(false));
+
+
+    const modificarAlto = (submenuHeight, ulPadre) => {
         while(ulPadre) {
             ulPadre.style.height = `${ulPadre.clientHeight + submenuHeight}px`;
-            let liPadre = ulPadre.parentElement;
-            ulPadre = liPadre.closest('.nav__submenu');
+            let liContenedor = ulPadre.parentElement;
+            ulPadre = liContenedor.closest('.nav__submenu');
         };
-    };
-
-    function clickMobile(btn) {
-        let height = 0;
-        const submenu = btn.nextElementSibling;
-        const submenuPadre = btn.parentElement;
-        let ulPadre = submenuPadre.closest('.nav__submenu');
-        if(!btn.classList.contains('arrow')) {
-            btn.classList.add('arrow');
-            submenu.style.height = `${submenu.scrollHeight}px`;
-            if(ulPadre) {
-                modificarAlto(submenu.scrollHeight, ulPadre)
-            };
-            return;
-        };
-        btn.classList.remove('arrow');
-        submenu.style.height = `${height}px`;
-        if(ulPadre) {
-            modificarAlto(-submenu.scrollHeight, ulPadre)
-        };
-    };
-
-    function clickDesktop() {
-        
     }
 
-    listaBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            manejarEvento(btn);
-        }); 
-    });
+    const menuMobile = (btn) => {
+        console.log('entre a funcion menuMobile')
+        const submenu = btn.nextElementSibling;
+        const liPadre = btn.parentElement;
+        let ulPadre = liPadre.closest('.nav__submenu');
+
+        let height = submenu.scrollHeight;
+
+        if(!btn.classList.contains('arrow')) {
+            btn.classList.add('arrow');
+            submenu.style.height = `${height}px`;
+            if(ulPadre) modificarAlto(height, ulPadre);
+        } else {
+            btn.classList.remove('arrow');
+            submenu.style.height = '0px';
+            if(ulPadre) modificarAlto(-height, ulPadre);
+        };
+    };
+
+    const recorrer = (submenu, liHermano, anterior) => {
+        while(liHermano) {
+            const ulHijos = liHermano.querySelectorAll('.nav__submenu');
+            if(ulHijos.length > 0) {
+                ulHijos.forEach(ulHijo => ulHijo.style.display = 'none');
+            } else {
+                liHermano.addEventListener('mouseover', () => {
+                    submenu.style.display = 'none';
+                });
+            };
+            liHermano = anterior ? liHermano.previousElementSibling:liHermano.nextElementSibling; 
+        };
+    }
+
+    const menuDesktop = (btn) => {
+        const submenu = btn.nextElementSibling,
+        liPadre = btn.parentElement;
+        let hermanoAnteriorLi = liPadre.previousElementSibling,
+        hermanoPosteriorLi = liPadre.nextElementSibling;
+        
+        submenu.style.display = 'block';
+        submenu.querySelectorAll('.nav__submenu').forEach(ulHijo => ulHijo.style.display = 'none');
+        
+        recorrer(submenu, hermanoAnteriorLi, true); 
+        recorrer(submenu, hermanoPosteriorLi, false); 
+    };
+
+    if (window.matchMedia("(min-width: 1280px)").matches) {
+        console.log('desktop')
+        listaBtns.forEach(btn => btn.addEventListener('mouseover', () => menuDesktop(btn)));
+        listaNav.addEventListener('mouseleave', () => {
+            listaNav.querySelectorAll('.nav__submenu').forEach(ulHijo => ulHijo.style.display = 'none');
+        })
+    } else {
+        console.log('mobile')
+        listaBtns.forEach(btn => btn.addEventListener('click', () => menuMobile(btn)));
+    };
 });
+
+
 
 
 
