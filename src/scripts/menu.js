389fd3348c0bdc17fb942043b424690c3a27e36c
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-08-19 13:05:31 -0300

    ajustes responsive

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -18,12 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             let liContenedor = ulPadre.parentElement;
             ulPadre = liContenedor.closest('.nav__submenu');
         };
-    }
+    };
 
     const menuMobile = (btn) => {
-        console.log('entre a funcion menuMobile')
-        const submenu = btn.nextElementSibling;
-        const liPadre = btn.parentElement;
+        const submenu = btn.nextElementSibling,
+        liPadre = btn.parentElement;
         let ulPadre = liPadre.closest('.nav__submenu');
 
         let height = submenu.scrollHeight;
@@ -51,43 +50,34 @@ document.addEventListener('DOMContentLoaded', () => {
             };
             liHermano = anterior ? liHermano.previousElementSibling:liHermano.nextElementSibling; 
         };
-    }
+    };
 
     const menuDesktop = (btn) => {
         const submenu = btn.nextElementSibling,
         liPadre = btn.parentElement;
         let hermanoAnteriorLi = liPadre.previousElementSibling,
         hermanoPosteriorLi = liPadre.nextElementSibling;
-        
         submenu.style.display = 'block';
         submenu.querySelectorAll('.nav__submenu').forEach(ulHijo => ulHijo.style.display = 'none');
-        
         recorrer(submenu, hermanoAnteriorLi, true); 
         recorrer(submenu, hermanoPosteriorLi, false); 
     };
 
     if (window.matchMedia("(min-width: 1280px)").matches) {
-        console.log('desktop')
         listaBtns.forEach(btn => btn.addEventListener('mouseover', () => menuDesktop(btn)));
-
         let cerrarMenuTimeout;
-
         menuBox.addEventListener('mouseleave', () => {
             cerrarMenuTimeout = setTimeout(() => {
                 menuBox.querySelectorAll('.nav__submenu').forEach(ulHijo => ulHijo.style.display = 'none');
             }, 300); 
         });
-
         menuBox.addEventListener('mouseenter', () => {
             clearTimeout(cerrarMenuTimeout);
         });
-
     } else {
         console.log('mobile')
         listaBtns.forEach(btn => btn.addEventListener('click', () => menuMobile(btn)));
     };
-
-
 });
 
 
