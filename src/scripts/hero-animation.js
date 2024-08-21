c09363464390c8f792070a210525e5a1efe9000a
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-08-20 21:15:03 -0300

    ajustes en hero-animation.js

--- src/scripts/hero-animation.js
+++ src/scripts/hero-animation.js
@@ -3,22 +3,21 @@ import SplitType from 'split-type'
 
 const textoHero = new SplitType('.header__hero-heading', { types: 'chars' });
 
-
 gsap.from('.char', {
-    x: 50,               // Desplazar hacia abajo
+    x: 100,               // Desplazar hacia abajo
     opacity: 0,          // Comenzar invisible
-    stagger: 0.06,       // Intervalo entre animaciones de caracteres
-    duration: 0.6,       // Duración de la animación
-    ease: "power3.out"   // Tipo de easing para la animación
+    stagger: 0.03,       // Intervalo entre animaciones de caracteres
+    duration: 0.8,       // Duración de la animación
+    ease: "power3.out",   // Tipo de easing para la animación
 })
 
 gsap.from('.btn-hero', {
     y: 100,               
     opacity: 0,          
-    stagger: 0.03,      
-    duration: 0.6,      
+    stagger: 0.6,      
+    duration: 0.8,      
     ease: "power3.out",
-    delay: 0.6, 
-    
+    delay: 0.7, 
 })
+
 
